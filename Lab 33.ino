4b6b13ca51768b4b80daa4c90462d0b27a1e0bc7
John Rey Pateno <99241491+JRPateno@users.noreply.github.com>
2024-02-29 12:32:31 +0800

    Create Lab 33.ino

---
+++ Lab 33.ino
@@ -0,0 +1,42 @@
+const int buttonPin = 1;
+
+int i = 2;
+int buttonState = 0;
+
+int delayVal = 0;
+
+void setup(){
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(2,OUTPUT);
+}
+
+
+void loop(){  
+  digitalWrite(i,HIGH);
+  delay(delayVal);
+  digitalWrite(i,LOW);
+  
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH){
+    i = i - 1;
+    if(i < 2){
+    i = 11;
+  	}
+  }
+  
+  else{
+    i = i + 1;
+    if(i > 11){
+    i = 2;
+  	}
+  }
+}
